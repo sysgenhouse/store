01f5d8ecfa481bdecd0c76f2a7b7c38997bd05bf
sysgenhousegg <87147041+sysgenhousegg@users.noreply.github.com>
2025-11-06 08:17:33 -0500

    Actualizacion

---
+++ inventario/src/main/java/com/linktic/tecnica/inventario/config/CorsConfig.java
@@ -0,0 +1,33 @@
+package com.linktic.tecnica.inventario.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/api/**", getCorsConfiguration());
+        return source;
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        return new CorsFilter(corsConfigurationSource());
+    }
+
+    private org.springframework.web.cors.CorsConfiguration getCorsConfiguration() {
+        org.springframework.web.cors.CorsConfiguration configuration = new org.springframework.web.cors.CorsConfiguration();
+        configuration.setAllowCredentials(true);
+        configuration.addAllowedOrigin("http://localhost:4200");
+        configuration.addAllowedOrigin("http://localhost:4201");
+        configuration.addAllowedHeader("*");
+        configuration.setAllowedMethods(java.util.Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        return configuration;
+    }
+}
